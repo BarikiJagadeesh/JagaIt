4f5218299ee0abb9bdee933734ea6f5ed8a7994c
jagadeesh <vyomanuatjagadeesh@gmail.com>
2024-07-21 18:54:46 +0530

    Prepare for deployment

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import styles from './Home.module.css';
-import Jaga from '../../Assets/Jagadeesh.png'
+// import Jaga from '../../Assets/Jagadeesh.png'
+import ShopKeeper from '../../Assets/HomeBackground.png'
 
 const Home = () => {
   return (
     <div className={styles.HomeContainer} id='Home'>
-      <p className={styles.HomeHeading}>Professional Website Design for Your Business</p>
-      <p className={styles.subHeading}><h3>Welcome to Jagadeesh IT Solutions</h3> <br/>Transforming Your Online Presence with Custom, Professional Web Design</p>
-      <p>We believe that every small business deserves a strong online presence. Our mission is to help you succeed in the digital landscape by creating a professional and effective online platform.</p>
-      <div className={styles.HomePhotos}>
+      <p className={styles.HomeHeading}>Professional Website Design for Your Business </p>
+      <div className={styles.HomeDataContainer}>
+        <div className={styles.HomeTextData}>
+        
+          <p className={styles.subHeading}> <br/>Transforming Your Online Presence with Custom, Professional Web Design</p>
+
+          <a href='https://wa.me/916300963789' className={styles.GetButton} target='_blank' rel="noreferrer">Get Started </a>
+         
+          </div>
+        <div className={styles.HomeImageContainer}>
+          <img src={ShopKeeper} alt='Shop Keeper'/>
+        </div>
+      </div>
+      {/* <div className={styles.HomePhotos}>
           <img src={Jaga} alt='Jagadeesh-Photo' className={styles.JagaPhoto}/>
           <p className={styles.photoName}>B Jagadeesh (MCA)</p>
           <p className={styles.photoDesig}><i>Web Developer</i></p>
-      </div>
+      </div> */}
       
     </div>
   )
